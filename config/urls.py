011d5d3df372bfa66ecde57bc6c693b015865f18
askvart <tur1amant@gmail.com>
2021-10-27 13:21:01 +0300

    urls path

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,4 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -24,9 +8,9 @@ from goods.views import GoodsListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'goods/$', GoodsListView.as_view(), name="goods-list"),
-    url(r'docs/', include_docs_urls(title="Admiring Fresh Students"))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('goods/', GoodsListView.as_view(), name="goods-list"),
+    path('docs/', include_docs_urls(title="Admiring Fresh Students"))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
