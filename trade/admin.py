4f3afe6f1b1963219dcbefa54c27d83bc6663b54
askvart <tur1amant@gmail.com>
2021-10-27 13:29:48 +0300

    admin

--- trade/admin.py
+++ trade/admin.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 from django.contrib import admin
 from .models import ShoppingCart, OrderInfo, OrderGoods
 
@@ -13,13 +10,5 @@ class OrderInfoAdmin(admin.ModelAdmin):
     list_display = ["user", "order_sn", "trade_no", "pay_status", "post_script", "order_mount",
                     "order_mount", "pay_time", "add_time"]
 
-    '''class OrderGoodsInline(object):
-        model = OrderGoods
-        exclude = ['add_time', ]
-        extra = 1
-        style = 'tab'
-
-    inlines = [OrderGoodsInline, ]
-'''
 
 
